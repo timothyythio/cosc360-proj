0a342a07f582c4d8509c295cef97055fc310c60e
EddyTian58 <bowentian58@gmail.com>
2025-02-28 23:54:49 -0800

    implemented liking functionality, refactored read more function

--- scripts/feed.js
+++ scripts/feed.js
@@ -42,7 +42,50 @@ pictureFile2 = "missile.png";
 bodyText2 = "lorem ipsum  blah blah The missile knows where it is at all times. It knows this because it knows where it isn't. By subtracting where it is from where it isn't, or where it isn't from where it is (whichever is greater), it obtains a difference, or deviation. The guidance subsystem uses deviations to generate corrective commands to drive the missile from a position where it is to a position where it isn't, and arriving at a position where it wasn't, it now is. Consequently, the position where it is, is now the position that it wasn't, and it follows that the position that it was, is now the position that it isn't. In the event that the position that it is in is not the position that it wasn't, the system has acquired a variation, the variation being the difference between where the missile is, and where it wasn't. If variation is considered to be a significant factor, it too may be corrected by the GEA. However, the missile must also know where it was. The missile guidance computer scenario works as follows. Because a variation has modified some of the information the missile has obtained, it is not sure just where it is. However, it is sure where it isn't, within reason, and it knows where it was. It now subtracts where it should be from where it wasn't, or vice-versa, and by differentiating this from the algebraic sum of where it shouldn't be, and where it was, it is able to obtain the deviation and its variation, which is called error.";
 
 numLikes = 99999;
-timePosted = new Date().toString(); 
+timePosted = new Date().toLocaleString("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+});
+
+function createReadMoreBtn(postCard, bodyText) {
+    let readMoreBtn = postCard.querySelector(".read-more-btn");
+    let postText = postCard.querySelector(".post-text");
+    let isExpanded = false;
+
+    readMoreBtn.addEventListener("click", () => {
+        if (isExpanded) {
+            postText.innerText = bodyText.substring(0, 500) + "...";
+            readMoreBtn.innerText = "Read More";
+            isExpanded = false;
+        } else {
+            postText.innerText = bodyText;
+            readMoreBtn.innerText = "Show Less";
+            isExpanded = true;
+        }
+    });
+}
+
+function createLikeBtn(postCard, numLikes, timePosted, authorName, poster) {
+    let isLiked = false;
+    let likesIcon = postCard.querySelector(".likesIcon");
+    let likeCountSpan = postCard.querySelector(".like-count");
+
+    likesIcon.addEventListener("click", function () {
+        if (!isLiked) {
+            numLikes++;
+            likesIcon.src = "../assets/liked.png"; // Change to the liked icon image
+            isLiked = true;
+        } else {
+            numLikes--;
+            likesIcon.src = "../assets/like.png"; // Change back to the original like icon
+            isLiked = false;
+        }
+        likeCountSpan.innerText = numLikes; // Update only the like count
+    });
+}
 
 function createTextCard(title, bodyText, numLikes, timePosted, authorName, post, poster) {
     let postCard = document.createElement("div");
@@ -58,25 +101,12 @@ function createTextCard(title, bodyText, numLikes, timePosted, authorName, post,
         </div>
         <div class="cardFooter">
             <img src="../assets/like.png" alt="like icon" class="likesIcon">
-            <p>${numLikes} Likes - ${timePosted} posted by <a href="${poster}.html">${authorName}</a></p>
+            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="${poster}.html">${authorName}</a></p>
         </div>
     `;
 
-    let readMoreBtn = postCard.querySelector(".read-more-btn");
-    let postText = postCard.querySelector(".post-text");
-    let isExpanded = false; 
-
-    readMoreBtn.addEventListener("click", () => {
-        if (isExpanded) {
-            postText.innerText = bodyText.substring(0, 500) + "...";
-            readMoreBtn.innerText = "Read More";
-            isExpanded = false;
-        } else {
-            postText.innerText = bodyText;
-            readMoreBtn.innerText = "Show Less";
-            isExpanded = true;
-        }
-    });
+    createReadMoreBtn(postCard, bodyText);
+    createLikeBtn(postCard, numLikes, timePosted, authorName, poster);
 
     document.getElementById("content").appendChild(postCard);
 }
@@ -98,25 +128,12 @@ function createPhotoCard(title, bodyText, numLikes, timePosted, authorName, post
         </div>
         <div class="cardFooter">
             <img src="../assets/like.png" alt="like icon" class="likesIcon">
-            <p>${numLikes} Likes - ${timePosted} posted by <a href="${poster}.html">${authorName}</a></p>
+            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="${poster}.html">${authorName}</a></p>
         </div>
     `;
 
-    let readMoreBtn = postCard.querySelector(".read-more-btn");
-    let postText = postCard.querySelector(".post-text");
-    let isExpanded = false; 
-
-    readMoreBtn.addEventListener("click", () => {
-        if (isExpanded) {
-            postText.innerText = bodyText.substring(0, 500) + "...";
-            readMoreBtn.innerText = "Read More";
-            isExpanded = false;
-        } else {
-            postText.innerText = bodyText;
-            readMoreBtn.innerText = "Show Less";
-            isExpanded = true;
-        }
-    });
+    createReadMoreBtn(postCard, bodyText);
+    createLikeBtn(postCard, numLikes, timePosted, authorName, poster);
 
     document.getElementById("content").appendChild(postCard);
 }
