0f294dbee309e598867b7a862d9446b65b70af3f
EddyTian58 <bowentian58@gmail.com>
2025-03-23 23:52:35 -0700

    made feed page work with server

--- scripts/feed.js
+++ scripts/feed.js
@@ -1,64 +1,13 @@
-var title;
-var bodyText;
-var numLikes;
-var timePosted;
-var authorName;
-var post;
-var poster;
-var topic;
-
-//first post example
-title = "Matcha Cafe Rave!!";
-authorName = "CoffeeLover99";
-post = "post";
-poster = "profile2";
-pictureFile = "matcha.jpg";
-bodyText = `GUUUUUUUYYYSSSSSSS
-
-I went to get boba at the place I always go to and get my usual order - my ankles break
-I literally ended up eating dirt and my drink splattered against everything in the shop.
-My hair lowkey got soaked and I legit just had to pick myself up and sit in my drink.
-I WAS SO TEMPTED TO LEAVE AND CRY but I felt so bad that every counter top, wall, and
-their mother fell victim to my clumsiness. What a waste of $9
-#CryingForEternity #SomeoneVenmoMe9Dollars #MyPoorBabyBoba
-
-TT
-
-I kept apologizing over and over, and I literally was about to get a mountain of paper towel
-from their bathroom to clean, but THANKFULLY a very kind angel of a human being that worked
-there told me it was okay and that they’ve had messier messes to clean (not sure how??) and
-that they will properly mop it up and such. BLESS and shout out to that worker
-(lets all collectively say “Thank you Jina~”).
-BUT THATS NOT ALL
-A VERY VERY HANDSOME MAN (Cha Eun-Woo level I’m talkin here guys) WHO WATCHED THE ENTIRE THING
-OFFERED TO BUY ME A NEW ONE??!!? He insisted, and wished me a good day.
-#LifeIsGood #ChaEunWooTalkedToMe #Fate #DoIPropose`;
-
-//second post example
-title2 = "This is a Test Text-Only Post";
-authorName2 = localStorage.getItem("loggedInUser");
-post2 = "post2";
-poster2 = "profile";
-pictureFile2 = "missile.png";
-bodyText2 = "lorem ipsum  blah blah The missile knows where it is at all times. It knows this because it knows where it isn't. By subtracting where it is from where it isn't, or where it isn't from where it is (whichever is greater), it obtains a difference, or deviation. The guidance subsystem uses deviations to generate corrective commands to drive the missile from a position where it is to a position where it isn't, and arriving at a position where it wasn't, it now is. Consequently, the position where it is, is now the position that it wasn't, and it follows that the position that it was, is now the position that it isn't. In the event that the position that it is in is not the position that it wasn't, the system has acquired a variation, the variation being the difference between where the missile is, and where it wasn't. If variation is considered to be a significant factor, it too may be corrected by the GEA. However, the missile must also know where it was. The missile guidance computer scenario works as follows. Because a variation has modified some of the information the missile has obtained, it is not sure just where it is. However, it is sure where it isn't, within reason, and it knows where it was. It now subtracts where it should be from where it wasn't, or vice-versa, and by differentiating this from the algebraic sum of where it shouldn't be, and where it was, it is able to obtain the deviation and its variation, which is called error.";
-
-//third post example
-title3 = "This is a Test Topic Post";
-authorName3 = "Shirakami Fubuki";
-post3 = "post3";
-poster3 = "profile";
-pictureFile3 = "foob.jpg";
-bodyText3 = "Glasses are really versatile. First, you can have glasses-wearing girls take them off and suddenly become beautiful, or have girls wearing glasses flashing those cute grins, or have girls stealing the protagonist's glasses and putting them on like, \"Haha, got your glasses!\" That's just way too cute! Also, boys with glasses! I really like when their glasses have that suspicious looking gleam, and it's amazing how it can look really cool or just be a joke. I really like how it can fulfill all those abstract needs. Being able to switch up the styles and colors of glasses based on your mood is a lot of fun too! It's actually so much fun! You have those half rim glasses, or the thick frame glasses, everything! It's like you're enjoying all these kinds of glasses at a buffet. I really want Luna to try some on or Marine to try some on to replace her eyepatch. We really need glasses to become a thing in hololive and start selling them for HoloComi. Don't. You. Think. We. Really. Need. To. Officially. Give. Everyone. Glasses?";
-topic = "Hololive"
-
-numLikes = 99999;
-timePosted = new Date().toLocaleString("en-US", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-});
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+}
 
 function createReadMoreBtn(postCard, bodyText) {
     let readMoreBtn = postCard.querySelector(".read-more-btn");
@@ -78,106 +27,143 @@ function createReadMoreBtn(postCard, bodyText) {
     });
 }
 
-function createLikeBtn(postCard, numLikes, timePosted, authorName, poster) {
+function createLikeBtn(postCard, numLikes, authorName, postId) {
     let isLiked = false;
     let likesIcon = postCard.querySelector(".likesIcon");
     let likeCountSpan = postCard.querySelector(".like-count");
+    let currentLikes = parseInt(numLikes) || 0;
 
     likesIcon.addEventListener("click", function () {
         if (!isLiked) {
-            numLikes++;
-            likesIcon.src = "../assets/liked.png"; // Change to the liked icon image
+            currentLikes++;
+            likesIcon.src = "../assets/liked.png"; 
             isLiked = true;
         } else {
-            numLikes--;
-            likesIcon.src = "../assets/like.png"; // Change back to the original like icon
+            currentLikes--;
+            likesIcon.src = "../assets/like.png"; 
             isLiked = false;
         }
-        likeCountSpan.innerText = numLikes; // Update only the like count
+        likeCountSpan.innerText = currentLikes; 
+
     });
 }
 
-function createTextCard(title, bodyText, numLikes, timePosted, authorName, post, poster) {
+function createTextCard(post) {
     let postCard = document.createElement("div");
     postCard.className = "postCard";
+    
+    const timePosted = formatDate(post.created_at);
+    const numLikes = 0; 
 
     postCard.innerHTML = `
         <div class="cardTitle">
-            <h1><a href="${post}.html">${title}</a></h1>
+            <h1><a href="post.php?id=${post.post_id}">${post.title}</a></h1>
         </div>
         <div class="textCardBody">
-            <p class="post-text">${bodyText.substring(0, 500)}...</p> 
+            <p class="post-text">${post.content.substring(0, 500)}...</p> 
             <span class="read-more-btn">Read More</span>
         </div>
         <div class="cardFooter">
             <img src="../assets/like.png" alt="like icon" class="likesIcon">
-            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="${poster}.html">${authorName}</a></p>
+            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="profile.php?username=${post.username}">${post.username}</a></p>
         </div>
     `;
 
-    createReadMoreBtn(postCard, bodyText);
-    createLikeBtn(postCard, numLikes, timePosted, authorName, poster);
+    createReadMoreBtn(postCard, post.content);
+    createLikeBtn(postCard, numLikes, post.username, post.post_id);
 
     document.getElementById("content").appendChild(postCard);
 }
 
-function createPhotoCard(title, bodyText, numLikes, timePosted, authorName, post, poster, pictureFile) {
+function createPhotoCard(post) {
     let postCard = document.createElement("div");
     postCard.className = "postCard";
+    
+    const timePosted = formatDate(post.created_at);
+    const numLikes = 0; 
+    const imagePath = post.image_path || "../assets/default-post-image.jpg";
 
     postCard.innerHTML = `
         <div class="cardTitle">
-            <h1><a href="${post}.html">${title}</a></h1>
+            <h1><a href="post.php?id=${post.post_id}">${post.title}</a></h1>
         </div>
         <div class="postPictureContainer">
-            <img src="../assets/${pictureFile}" class="postPicture" alt="Post Picture">
+            <img src="${imagePath}" class="postPicture" alt="Post Picture">
         </div>
         <div class="photoCardBody">
-            <p class="post-text">${bodyText.substring(0, 500)}...</p> 
+            <p class="post-text">${post.content.substring(0, 500)}...</p> 
             <span class="read-more-btn">Read More</span>
         </div>
         <div class="cardFooter">
             <img src="../assets/like.png" alt="like icon" class="likesIcon">
-            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="${poster}.html">${authorName}</a></p>
+            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="profile.php?username=${post.username}">${post.username}</a></p>
         </div>
     `;
 
-    createReadMoreBtn(postCard, bodyText);
-    createLikeBtn(postCard, numLikes, timePosted, authorName, poster);
+    createReadMoreBtn(postCard, post.content);
+    createLikeBtn(postCard, numLikes, post.username, post.post_id);
 
     document.getElementById("content").appendChild(postCard);
 }
 
-function createTopicCard(title, bodyText, numLikes, timePosted, authorName, post, poster, pictureFile, topic) {
+function createTopicCard(post) {
     let postCard = document.createElement("div");
     postCard.className = "postCard";
+    
+    const timePosted = formatDate(post.created_at);
+    const numLikes = 0; 
+    const imagePath = post.image_path || "../assets/default-post-image.jpg";
+    const topicName = post.topic_name || "General";
 
     postCard.innerHTML = `
         <div class="cardTitle">
-            <h1><a href="${post}.html">${title}</a></h1>
-            <h2 class="topicName"><a href=${topic}.html>Topic: ${topic}</a></h2>
+            <h1><a href="post.php?id=${post.post_id}">${post.title}</a></h1>
+            <h2 class="topicName"><a href="topic.php?name=${topicName}">Topic: ${topicName}</a></h2>
         </div>
         <div class="topicPostPictureContainer">
-            <img src="../assets/${pictureFile}" class="postPicture" alt="Post Picture">
+            <img src="${imagePath}" class="postPicture" alt="Post Picture">
         </div>
         <div class="topicCardBody">
-            <p class="post-text">${bodyText.substring(0, 500)}...</p> 
+            <p class="post-text">${post.content.substring(0, 500)}...</p> 
             <span class="read-more-btn">Read More</span>
         </div>
         <div class="cardFooter">
             <img src="../assets/like.png" alt="like icon" class="likesIcon">
-            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="${poster}.html">${authorName}</a></p>
+            <p><span class="like-count">${numLikes}</span> Likes - ${timePosted} posted by <a href="profile.php?username=${post.username}">${post.username}</a></p>
         </div>
     `;
 
-    createReadMoreBtn(postCard, bodyText);
-    createLikeBtn(postCard, numLikes, timePosted, authorName, poster);
+    createReadMoreBtn(postCard, post.content);
+    createLikeBtn(postCard, numLikes, post.username, post.post_id);
 
     document.getElementById("content").appendChild(postCard);
 }
 
-createPhotoCard(title, bodyText, numLikes, timePosted, authorName, post, poster, pictureFile);
-createTextCard(title2, bodyText2, numLikes, timePosted, authorName2, post2, poster2, pictureFile2);
-createTopicCard(title3, bodyText3, numLikes, timePosted, authorName3, post2, poster3, pictureFile3, topic);
+function displayPosts() {
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "";
+    
+    if (typeof postsData !== 'undefined' && postsData.length > 0) {
+        postsData.forEach(post => {
+            if (post.topic_name && post.image_path) {
+                createTopicCard(post);
+            } else if (post.image_path) {
+                createPhotoCard(post);
+            } else {
+                createTextCard(post);
+            }
+        });
+    } else {
+        contentDiv.innerHTML = `
+            <div class="no-posts">
+                <h2>No posts available</h2>
+                <p>Be the first to create a post!</p>
+                <a href="new-post.html" class="create-post-btn">Create Post</a>
+            </div>
+        `;
+    }
+}
 
-
+document.addEventListener("DOMContentLoaded", function() {
+    displayPosts();
+});
