9d343cb2f329cea577e5a4c5ff100e27cbe29cf3
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-04-10 22:28:12 -0700

    Added error display on each page

--- pages/topic.php
+++ pages/topic.php
@@ -2,6 +2,10 @@
 session_start();
 require_once '../sql/db_connect.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // get topic ID 
 $topic_id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
